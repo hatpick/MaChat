234f0632cebd0edd98d0944bac9e3e79f235750a
Soroush <hatpick@gmail.com>
2015-07-02 21:03:46 -0700

    Translucent

--- app/src/main/java/datapp/machat/activity/LoginActivity.java
+++ app/src/main/java/datapp/machat/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
